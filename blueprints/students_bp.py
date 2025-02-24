0df7317be918330c3c5fb9959d723d2b8307b758
Angie <angel890817@hotmail.com>
2025-02-24 14:05:32 +1100

    Get All Students

---
+++ blueprints/students_bp.py
@@ -0,0 +1,59 @@
+from flask import Blueprint
+from init import db
+from models.students import Student, many_students
+
+students_bp = Blueprint('students', __name__)
+
+# Read all - GET /students
+@students_bp.route('/students')
+def get_all_students():
+    stmt = db.select(Student)
+    students = db.session.scalars(stmt)
+    return many_students.dump(students)
+
+
+# Read one - GET / students/ <int:id>
+# Create - POST / students
+# Update - PUT / students / <int:id>
+# Delete - DELETE/ students / <int:id>
+
+# possible extra route
+# Enrol - Post / students/ <int:student_id>/ <int:course_id>
+# Unenrol - DELETE/ students/ <int:student_id>/ <int:course_id>
+
+
+#  update
+# @student_dp.route('/studentd/<int:student_id>', method = ['PUT', 'PATCH'])
+# def update_student(student_id):
+#     try:
+        
+#         # Fetch student by id
+#         stmt = db.select(Student).filter_by(id = student_id)
+#         student = db.session.scalar(stmt)
+#         if student:
+#             # Get incoming request body
+#             data = student_without_id.load(request.json)
+#             # update the attribute of the student with the incoming data
+#             student.name = data.get('name') or student.name
+#             email = data.get('email') or student.email
+#             address = data.get('address') or student.address
+
+#             db.session.commit()
+#             return one_student.dump(student)
+#     except IntegrityError as err:
+#         if err.orig.pgcode == errorcodes.UNIQUE_VIOLATION:
+#             return{'error': f''}
+
+
+
+# delete
+# @students_bp.route('/students/<int:student_id>', method = ['DELETE'])
+# def delete_student(id):
+#     stmt = db.select(Student).filter_by(id = student_id)
+#     student = db.session.scalar(stmt)
+#     if student:
+#         db.session.delete(student)
+#         db.session.commit()
+#         return {}, 204
+#     else:
+#         return {'error': f'Student with id {student_id} does not exist'}, 404
--- models/students.py
+++ models/students.py
@@ -2,17 +2,21 @@ from init import db, ma
 
 class Student(db.Model):
     __tablename__ = 'students'
+    __table_args__ = {'schema': 'academy'}  # Specify the schema
 
-    id = db.Column(db.Interger, primary_key = True)
+
+    id = db.Column(db.Integer, primary_key = True)
 
     name = db.Column(db.String(100), nullable = False)
-    email = db.Column(db.String(200), nullable = False)
+    email = db.Column(db.String(200), nullable = False, unique = True)
     address = db.Column(db.String(250))
                         
-class Student(ma.Schema):
+class StudentSchema(ma.Schema):
     class Meta:
         fields = ('id', 'name', 'email', 'address')
 
 one_student = StudentSchema()
 many_students = StudentSchema(many = True)
+
+
 
